c42e97710289bc07867c353cfcbda0ff08596aea
Mitche Guevara <mitche.guevara@gmail.com>
2017-08-31 16:15:05 -0400

    adding a new vue

--- spa/src/App.vue
+++ spa/src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="app">
   <!-- the router outlet, where all matched components would ber viewed -->
+  <router-link v-bind:to="'/'">Home</router-link>
+  <router-link v-bind:to="'/about'">About</router-link>
+  <router-link v-bind:to="'/tips'">Tips</router-link>
+
   <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
+  name: 'app'
 }
 </script>
 <!-- styling for the component -->
--- spa/src/main.js
+++ spa/src/main.js
@@ -1,37 +1,45 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-//import the vue instance
+// import the vue instance
 import Vue from 'vue'
-//import the App component
+// import the App component
 import App from './App'
-//import the vue router
+// import the vue router
 import VueRouter from 'vue-router'
-//tell vue to use the router
+// tell vue to use the router
 Vue.use(VueRouter)
-//import the hello component
-//import the hello component
+// define your routes
+// import the hello component
+// import the hello component
 import Hello from './components/Hello'
-//import the about component
+// import the about component
 import About from './components/About'
-//define your routes
+
+import Tips from './components/Tips'
+// define your routes
 const routes = [
-//route for the home route of the web page
+// route for the home route of the web page
 { path: '/', component: Hello },
-//route for the about route of the web page
-{ path: '/about', component: About }
+// route for the about route of the web page
+{ path: '/about', component: About },
+{ path: '/tips', component: Tips }
 ]
+
+// Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
 const router = new VueRouter({
   routes, // short for routes: routes
   mode: 'history'
 })
-//instatinat the vue instance
+// instatinat the vue instance
 new Vue({
-//define the selector for the root component
+  // define the selector for the root component
   el: '#app',
-  //pass the template to the root component
+  // pass the template to the root component
   template: '<App/>',
-  //declare components that the root component can access
+  // declare components that the root component can access
   components: { App },
-  //pass in the router to the Vue instance
+  // pass in the router to the Vue instance
   router
-}).$mount('#app')//mount the router on the app
+}).$mount('#app')// mount the router on the app
